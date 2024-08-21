d78ef19a505a67864473eb0a54c282bdd48b37db
Rahul <rahulpratik1212@gmail.com>
2024-08-21 10:11:51 +0530

    add chat functionality

--- 05-webRTC/app.js
+++ 05-webRTC/app.js
@@ -4,18 +4,43 @@ const http = require("http");
 const path = require("path");
 const socketIO = require("socket.io");
 
+const indexRouter = require("./routes");
+
 const server = http.createServer(app);
 const io = socketIO(server);
 
+let waitingusers = [];
+let rooms = {};
+
+io.on("connection", (socket) => {
+  socket.on("joinroom", () => {
+    if (waitingusers.length > 0) {
+      let partner = waitingusers.shift();
+      const roomname = `${socket.id}-${partner.id}`;
+      socket.join(roomname);
+      partner.join(roomname);
+
+      io.to(roomname).emit("joined", roomname)
+    } else {
+      waitingusers.push(socket);
+    }
+  });
+
+  socket.on("message", (data) => {
+    socket.broadcast.to(data.room).emit("message", data.message)
+  })
+
+  socket.on("disconnect", () => {
+    let index = waitingusers.findIndex((waitingusers) => waitingusers.id === socket.id)
+    waitingusers.splice(index, 1)
+  })
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", (socket) => {
-  console.log("Connected");
-});
+app.use("/", indexRouter);
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
-
-server.listen(3000)
+server.listen(3000);
